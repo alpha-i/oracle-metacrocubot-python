b8607c77aace8aa8410a558b284022050edd356a
Daniele Murroni <daniele.murroni@gmail.com>
2018-03-15 18:04:53 +0000

    Datasource and Test

---
+++ alphai_metacrocubot_oracle/datasource.py
@@ -0,0 +1,52 @@
+from datetime import timedelta
+
+import pandas as pd
+
+from alphai_delphi import AbstractDataSource
+
+
+class DataSource(AbstractDataSource):
+
+    def __init__(self, configuration):
+        raw_data_file = configuration['data_file']
+
+        dataframe = pd.DataFrame()
+
+        with pd.HDFStore(raw_data_file) as store:
+            dataframe = store.get('data')
+
+        features = set(dataframe.columns) - {'DateStamps', 'Ticker'}
+        self._data = {feature: dataframe.pivot(columns='Ticker', values=feature) for feature in features}
+
+    def get_data(self, current_datetime, interval):
+
+        start_date = current_datetime - interval
+        end_date = current_datetime
+
+        data = {
+            key: value[start_date:end_date] for key, value in self._data.items()
+        }
+
+        return data
+
+    def values_for_symbols_feature_and_time(self, symbol_list, feature, current_datetime):
+
+        try:
+            feature_data = self._data[feature]
+        except KeyError:
+            raise KeyError("Feature {} doesn't exists in data".format(feature))
+
+        symbol_data = feature_data[symbol_list]
+
+        return symbol_data.loc[current_datetime]
+
+
+
+
+    @property
+    def start(self):
+        pass
+
+    @property
+    def end(self):
+        pass
--- tests/test_metacrocubot.py
+++ tests/test_metacrocubot.py
@@ -16,101 +16,96 @@ OUTPUT_DIR = tempfile.TemporaryDirectory().name
 
 logging.basicConfig(level=logging.DEBUG)
 
-oracle_configuration = {
-        'prediction_delta': {
-            'unit': 'days',
-            'value': 10
-        },
-        'training_delta': {
-            'unit': 'days',
-            'value': 20
-        },
-        'prediction_horizon': {
-            'unit': 'days',
-            'value': 1
-        },
-        'data_transformation': {
-            'feature_config_list': [
-                {
-                    'name': 'close',
-                    'transformation': {
-                        'name': 'log-return'
-                    },
-                    'normalization': 'standard',
-                    'is_target': True,
-                    'local': False,
-                    'length': 5
-                },
-            ],
-            'features_ndays': 10,
-            'features_resample_minutes': 15,
-            'fill_limit': 5,
-        },
-        "model": {
-            'train_path': OUTPUT_DIR,
-            'covariance_method': 'NERCOME',
-            'covariance_ndays': 9,
-            'model_save_path': OUTPUT_DIR,
-            'tensorboard_log_path': OUTPUT_DIR,
-            'd_type': 'float32',
-            'tf_type': 32,
-            'random_seed': 0,
-
-            # Training specific
-            'predict_single_shares': True,
-            'n_epochs': 1,
-            'n_retrain_epochs': 1,
-            'learning_rate': 2e-3,
-            'batch_size': 100,
-            'cost_type': 'bayes',
-            'n_train_passes': 30,
-            'n_eval_passes': 100,
-            'resume_training': False,
-            'classify_per_series': False,
-            'normalise_per_series': False,
-
-            # Topology
-            'n_series': 3,
-            'n_assets': 3,
-            'n_features_per_series': 271,
-            'n_forecasts': 1,
-            'n_classification_bins': 12,
-            'layer_heights': [270, 270],
-            'layer_widths': [3, 3],
-            'activation_functions': ['relu', 'relu'],
-
-            # Initial conditions
-            'INITIAL_ALPHA': 0.2,
-            'INITIAL_WEIGHT_UNCERTAINTY': 0.4,
-            'INITIAL_BIAS_UNCERTAINTY': 0.4,
-            'INITIAL_WEIGHT_DISPLACEMENT': 0.1,
-            'INITIAL_BIAS_DISPLACEMENT': 0.4,
-            'USE_PERFECT_NOISE': True,
-
-            # Priors
-            'double_gaussian_weights_prior': False,
-            'wide_prior_std': 1.2,
-            'narrow_prior_std': 0.05,
-            'spike_slab_weighting': 0.5,
-        },
-        "universe": {
-            "method": "liquidity",
-            "n_assets": 3,
-            "ndays_window": 5,
-            "update_frequency": 'weekly',
-            "avg_function": 'median',
-            "dropna": False
-        },
-    }
-
 
 class TestMetaCrocubot(unittest.TestCase):
 
-    SIMULATION_START = datetime.datetime(2009, 1, 5, tzinfo=pytz.utc)
-    SIMULATION_END = datetime.datetime(2009, 1, 31, tzinfo=pytz.utc)
-    CALENDAR_NAME = 'NYSE'
-
     def test_run(self):
+        oracle_configuration = {
+            'prediction_delta': {
+                'unit': 'days',
+                'value': 10
+            },
+            'training_delta': {
+                'unit': 'days',
+                'value': 20
+            },
+            'prediction_horizon': {
+                'unit': 'days',
+                'value': 1
+            },
+            'data_transformation': {
+                'feature_config_list': [
+                    {
+                        'name': 'close',
+                        'transformation': {
+                            'name': 'log-return'
+                        },
+                        'normalization': 'standard',
+                        'is_target': True,
+                        'local': False,
+                        'length': 5
+                    },
+                ],
+                'features_ndays': 10,
+                'features_resample_minutes': 15,
+                'fill_limit': 5,
+            },
+            "model": {
+                'train_path': OUTPUT_DIR,
+                'covariance_method': 'NERCOME',
+                'covariance_ndays': 9,
+                'model_save_path': OUTPUT_DIR,
+                'tensorboard_log_path': OUTPUT_DIR,
+                'd_type': 'float32',
+                'tf_type': 32,
+                'random_seed': 0,
+
+                # Training specific
+                'predict_single_shares': True,
+                'n_epochs': 1,
+                'n_retrain_epochs': 1,
+                'learning_rate': 2e-3,
+                'batch_size': 100,
+                'cost_type': 'bayes',
+                'n_train_passes': 30,
+                'n_eval_passes': 100,
+                'resume_training': False,
+                'classify_per_series': False,
+                'normalise_per_series': False,
+
+                # Topology
+                'n_series': 3,
+                'n_assets': 3,
+                'n_features_per_series': 271,
+                'n_forecasts': 1,
+                'n_classification_bins': 12,
+                'layer_heights': [270, 270],
+                'layer_widths': [3, 3],
+                'activation_functions': ['relu', 'relu'],
+
+                # Initial conditions
+                'INITIAL_ALPHA': 0.2,
+                'INITIAL_WEIGHT_UNCERTAINTY': 0.4,
+                'INITIAL_BIAS_UNCERTAINTY': 0.4,
+                'INITIAL_WEIGHT_DISPLACEMENT': 0.1,
+                'INITIAL_BIAS_DISPLACEMENT': 0.4,
+                'USE_PERFECT_NOISE': True,
+
+                # Priors
+                'double_gaussian_weights_prior': False,
+                'wide_prior_std': 1.2,
+                'narrow_prior_std': 0.05,
+                'spike_slab_weighting': 0.5,
+            },
+            "universe": {
+                "method": "liquidity",
+                "n_assets": 3,
+                "ndays_window": 5,
+                "update_frequency": 'weekly',
+                "avg_function": 'median',
+                "dropna": False
+            },
+        }
 
         scheduling_configuration = {
             "prediction_frequency": {"frequency_type": "WEEKLY", "days_offset": 0, "minutes_offset": 75},
@@ -123,10 +118,14 @@ class TestMetaCrocubot(unittest.TestCase):
             scheduling_configuration=scheduling_configuration
         )
 
+        simulation_start = datetime.datetime(2009, 1, 5, tzinfo=pytz.utc)
+        simulation_end = datetime.datetime(2009, 1, 31, tzinfo=pytz.utc)
+        calendar_name = 'NYSE'
+
         scheduler = Scheduler(
-            self.SIMULATION_START,
-            self.SIMULATION_END,
-            self.CALENDAR_NAME,
+            simulation_start,
+            simulation_end,
+            calendar_name,
             oracle.prediction_frequency,
             oracle.training_frequency,
         )
@@ -164,3 +163,7 @@ class TestMetaCrocubot(unittest.TestCase):
 
         for filename in expected_files:
             self.assertTrue(os.path.isfile(os.path.join(OUTPUT_DIR, filename)))
+
+
+
+
